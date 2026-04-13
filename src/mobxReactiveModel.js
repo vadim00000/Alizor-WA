29625714455add50740fde689816b231283d35d2
Ali Najib <anajib@kth.se>
2026-04-13 03:54:38 +0200

    chore: remove duplicate train model after merge (keep trainModel + App entry)

--- src/mobxReactiveModel.js
+++
@@ -1,9 +0,0 @@
-import { observable, configure, reaction } from "mobx";
-configure({ enforceActions: "never", }); 
-import {model} from "/src/TrainModel.js";
-
-export const reactiveModel= observable(model)
-
-window.myModel= reactiveModel;
-
-
--- src/reactjs/main.jsx
+++ src/reactjs/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../index.css'
 import App from './App.jsx'
-import { reactiveModel } from "/src/mobxReactiveModel.js";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App model={reactiveModel} />
+    <App />
   </StrictMode>,
 )
